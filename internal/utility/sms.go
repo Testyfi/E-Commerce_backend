5248e6374f27970d81592511f0c7d5f252866db0
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-10-14 00:40:47 +0530

    minor changes

--- internal/utility/sms.go
+++ internal/utility/sms.go
@@ -14,11 +14,9 @@ func SMS(message string, variables []string, numbers []string) {
 	fmt.Println(message, varValues, numbers)
 	URL := fmt.Sprintf("https://www.fast2sms.com/dev/bulkV2?authorization=%s&route=dlt&sender_id=tstify&message=%s&variables_values=%sflash=0&numbers=%s", os.Getenv("SMS_KEY"), message, varValues, numbers[0])
 	resp, err := http.Get(URL)
-	fmt.Println(URL)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer resp.Body.Close()
-	fmt.Println(resp)
 }
