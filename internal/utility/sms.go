77ab78bb76f9e2d4046823a6487658684fa44958
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-10-14 00:03:02 +0530

    fixed OTP bug

--- internal/utility/sms.go
+++ internal/utility/sms.go
@@ -9,7 +9,7 @@ import (
 func SMS(message string, variables []string, numbers []string) {
 	var varValues string = ""
 	for i := 0; i < len(variables); i++ {
-		varValues += variables[i] + "|"
+		varValues += variables[i] + "%7C"
 	}
 	fmt.Println(message, varValues, numbers)
 	URL := fmt.Sprintf("https://www.fast2sms.com/dev/bulkV2?authorization=%s&route=dlt&sender_id=tstify&message=%s&variables_values=%sflash=0&numbers=%s", os.Getenv("SMS_KEY"), message, varValues, numbers[0])
